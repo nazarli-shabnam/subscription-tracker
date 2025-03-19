66cfa3bcb5c699349864e4bcaa7c44d7fddec7e5
nazarli-shabnam <shabnamnezerli@gmail.com>
2025-03-19 21:57:58 +0400

    reminder done

--- config/env.js
+++ config/env.js
@@ -2,8 +2,10 @@ import { config } from 'dotenv';
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-export const { PORT, NODE_ENV,
+export const { PORT, NODE_ENV,SERVER_URL,
     DB_URI, JWT_SECRET,JWT_EXPIRES_IN,
     ARCJET_ENV,ARCJET_KEY,
+    QSTASH_TOKEN,QSTASH_URL,
+    EMAIL_PASSWORD
 }=process.env;
 
---
+++ routes/workflow.routes.js
@@ -0,0 +1,7 @@
+import {Router} from 'express';
+import { sendReminders } from '../controllers/workflow.controller.js';
+
+const workflowRouter=Router();
+workflowRouter.post('/subscriptions/reminder',sendReminders);
+
+export default workflowRouter;
