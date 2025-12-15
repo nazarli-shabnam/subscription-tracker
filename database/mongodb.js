a6ae4121249a639123f1c501474d627c7d3fdc62
nazarli-shabnam <shabnamnezerli@gmail.com>
2025-12-15 20:57:01 +0400

    type

--- database/mongodb.js
+++ database/mongodb.js
@@ -3,7 +3,7 @@ import { DB_URI, NODE_ENV } from "../config/env.js";
 
 if (!DB_URI) {
   throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.<development/production>.local"
+    "Please define the DB_URI environment variable inside .env.<development/production>.local"
   );
 }
 
