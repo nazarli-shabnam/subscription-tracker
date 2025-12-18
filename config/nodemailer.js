29a6ada4ea1ee365cc2fb450e2a76757187e5e92
nazarli-shabnam <shabnamnezerli@gmail.com>
2025-12-18 10:24:26 +0400

    if the email user or password is not defined, throw an error

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,13 +1,17 @@
 import nodemailer from 'nodemailer';
 
-import { EMAIL_PASSWORD } from './env.js'
+import { EMAIL_PASSWORD, EMAIL_USER } from './env.js'
 
-export const accountEmail = 'sebnemnezerli13@gmail.com';
+if (!EMAIL_USER || !EMAIL_PASSWORD) {
+  throw new Error('EMAIL_USER and EMAIL_PASSWORD must be defined in environment variables');
+}
+
+export const accountEmail = EMAIL_USER;
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: accountEmail,
+    user: EMAIL_USER,
     pass: EMAIL_PASSWORD
   }
 })
