1db72d0139870ed31cb169e40549b33162826079
nazarli-shabnam <shabnamnezerli@gmail.com>
2025-12-18 10:14:35 +0400

    user notification preferences

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,10 +2,19 @@ import { Router } from 'express';
 
 import authorize from '../middlewares/auth.middleware.js'
 import { getUser, getUsers } from '../controllers/user.controller.js'
+import { getBudget, setBudget } from '../controllers/budget.controller.js'
+import { getNotificationPreferences, updateNotificationPreferences } from '../controllers/notification.controller.js'
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
+
+userRouter.get('/budget', authorize, getBudget);
+userRouter.post('/budget', authorize, setBudget);
+userRouter.put('/budget', authorize, setBudget);
+
+userRouter.get('/notifications', authorize, getNotificationPreferences);
+userRouter.put('/notifications', authorize, updateNotificationPreferences);
 
 userRouter.get('/:id', authorize, getUser);
 
