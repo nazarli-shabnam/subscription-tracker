0a16cfade3a1ab2b06a9962c3b32c802c2cf95ab
nazarli-shabnam <shabnamnezerli@gmail.com>
2025-12-19 01:13:12 +0400

    date validator

--- models/user.model.js
+++ models/user.model.js
@@ -20,6 +20,44 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'User Password is required'],
     minLength: 6,
+  },
+  notificationPreferences: {
+    reminderDays: {
+      type: [Number],
+      default: [7, 5, 2, 1],
+      validate: {
+        validator: (arr) => arr.every(day => day >= 0 && day <= 365),
+        message: 'Reminder days must be between 0 and 365'
+      }
+    },
+    emailEnabled: {
+      type: Boolean,
+      default: true
+    },
+    quietHours: {
+      enabled: {
+        type: Boolean,
+        default: false
+      },
+      start: {
+        type: String,
+        default: '22:00',
+        match: [/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/, 'Invalid time format. Use HH:mm']
+      },
+      end: {
+        type: String,
+        default: '08:00',
+        match: [/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/, 'Invalid time format. Use HH:mm']
+      }
+    },
+    noEmailDays: {
+      type: [Number],
+      default: [],
+      validate: {
+        validator: (arr) => arr.every(day => day >= 0 && day <= 6),
+        message: 'Day values must be 0-6 (Sunday-Saturday)'
+      }
+    }
   }
 }, { timestamps: true });
 
